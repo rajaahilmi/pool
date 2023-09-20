0e33cef9a0ba5141b1373c99dcba8acea8bf6277
Rajaa Hilmi <rhilmi@e1r4p2.1337.ma>
2023-09-20 16:18:54 +0100

    owl

--- c01/ex01/ft_ultimate_ft.c
+++ c01/ex01/ft_ultimate_ft.c
@@ -6,13 +6,13 @@
 /*   By: rhilmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 20:43:05 by rhilmi            #+#    #+#             */
-/*   Updated: 2023/09/17 21:43:55 by rhilmi           ###   ########.fr       */
+/*   Updated: 2023/09/20 12:43:36 by rhilmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
-void	f_ultimate_ft(int	*********nbr)
+void	ft_ultimate_ft(int	*********nbr)
 {
 	*********nbr = 42;
 }
