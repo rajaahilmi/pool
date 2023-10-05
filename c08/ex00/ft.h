4e0e73d130308c9e85a1cf9ec37c95c58ed5ff3d
Rajaa Hilmi <rhilmi@e2r5p10.1337.ma>
2023-10-05 19:35:03 +0100

    header files

---
+++ c08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhilmi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 19:34:09 by rhilmi            #+#    #+#             */
+/*   Updated: 2023/10/05 19:34:21 by rhilmi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
